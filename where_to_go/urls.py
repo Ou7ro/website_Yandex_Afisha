98baec37914c8a06dae98737c004d8d2f3473569
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-07-17 19:15:17 +0500

    Create places app

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-import views
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', index)
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
