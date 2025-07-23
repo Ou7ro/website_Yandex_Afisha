0bb36317ba867a1e642c2f80f09b76eb0d073564
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-07-24 00:43:26 +0500

    Add tinymce for Admin page

--- places/models.py
+++ places/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название', max_length=200)
     place_id = models.CharField('Id места', null=True, blank=True)
     description_short = models.TextField('Короткое описание')
-    description_long = models.TextField('Длинное описание')
+    description_long = HTMLField('Длинное описание')
     lng = models.FloatField('Долгота', null=True, blank=True)
     lat = models.FloatField('Широта', null=True, blank=True)
-    
+
     def __str__(self):
         return self.title
 
@@ -17,9 +18,9 @@ class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField('Изображение')
     position = models.PositiveIntegerField('Позиция', default=0)
-    
+
     def __str__(self):
         return f'{self.position} {self.place.title}'
-    
+
     class Meta:
         ordering = ['position']
